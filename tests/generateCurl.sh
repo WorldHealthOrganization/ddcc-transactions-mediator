2fce060ece14bcf8cc191b1c197dae32c6be0258
Carl Leitner <litlfred@gmail.com>
2021-05-21 02:16:40 -0400

    pretty print test output (at the expense of python)

--- tests/generateCurl.sh
+++ tests/generateCurl.sh
@@ -1,1 +1,2 @@
-curl -X POST localhost:4321/svc/generateHealthCertificate -d @Parameters-SVC-TX-SHE-Parameters-1.json -H "Content-type: application/fhir+json"
+curl -X POST localhost:4321/svc/generateHealthCertificate -d @Parameters-SVC-TX-SHE-Parameters-1.json -H "Content-type: application/fhir+json" | python -m json.tool  
+
